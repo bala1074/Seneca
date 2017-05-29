a644c58a571628abe782112f8f55d4a73d65e497
Balasatish Injarapu <injarapu@internal.synopsys.com>
2017-05-29 12:45:18 +0530

    2nd commit, basic adding product operation using seneca

--- math.js
+++ math.js
@@ -5,7 +5,18 @@ seneca.add({role:'math',cmd:'sum'},
 		respond(null,{answer:sum});
 	}
 );
+seneca.add({role:'math',cmd:'product'},
+	function onMatch(msg,respond){
+		var product=msg.left*msg.right;
+		respond(null,{answer:product});
+	}
+);
 seneca.act({role:'math',cmd:'sum',left:11,right:2},
+	function onResponse(err,result){
+		if(err) return console.log(err);
+		console.log(result);
+	}
+).act({role:'math',cmd:'product',left:12,right:5},
 	function onResponse(err,result){
 		if(err) return console.log(err);
 		console.log(result);
