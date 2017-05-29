c978c2fb6bcf8670c1bc303c01e04f1fa1d84d6a
Balasatish Injarapu <injarapu@internal.synopsys.com>
2017-05-29 12:51:26 +0530

    3rd commit, updating seneca add operations using seneca

--- math.js
+++ math.js
@@ -1,22 +1,26 @@
-var seneca=require('seneca')();
-seneca.add({role:'math',cmd:'sum'},
-	function onMatch(msg,respond){
-		var sum=msg.left+msg.right;
-		respond(null,{answer:sum});
-	}
-);
-seneca.add({role:'math',cmd:'product'},
-	function onMatch(msg,respond){
-		var product=msg.left*msg.right;
-		respond(null,{answer:product});
-	}
-);
+var seneca=require('seneca')()
+			.use(math);
+
+function math(operations){
+	this.add('role:math,cmd:sum',
+		function onMatch(msg,respond){
+			var sum=msg.left+msg.right;
+			respond(null,{answer:sum});
+		}
+	);
+	this.add('role:math,cmd:product',
+		function onMatch(msg,respond){
+			var product=msg.left*msg.right;
+			respond(null,{answer:product});
+		}
+	);
+}
 seneca.act({role:'math',cmd:'sum',left:11,right:2},
 	function onResponse(err,result){
 		if(err) return console.log(err);
 		console.log(result);
 	}
-).act({role:'math',cmd:'product',left:12,right:5},
+).act({role:'math',cmd:'product',left:13,right:5},
 	function onResponse(err,result){
 		if(err) return console.log(err);
 		console.log(result);
